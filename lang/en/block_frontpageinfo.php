a965a553dd854d641566d28865aab3a8bc9660c9
Sarah Cotton <sarah.cotton@solent.ac.uk>
2019-06-17 11:44:49 +0100

    Initial commit

---
+++ lang/en/block_frontpageinfo.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Front page info';
+$string['frontpageinfo'] = '';
+$string['frontpageinfo:addinstance'] = 'Add a new Library Opening Times block';
+$string['frontpageinfo:myaddinstance'] = 'Add a new Library Opening Times block to the My Moodle page';
