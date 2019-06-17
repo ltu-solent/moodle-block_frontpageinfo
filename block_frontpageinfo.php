f7e61e401b9f8ef037983d8bbb3d2fbcda570b49
Oliver Kent <o.jkent@gmail.com>
2019-06-17 14:30:57 +0100

    CSS changes

--- block_frontpageinfo.php
+++ block_frontpageinfo.php
@@ -3,12 +3,12 @@ class block_frontpageinfo extends block_base {
     public function init() {
         $this->title = get_string('frontpageinfo', 'block_frontpageinfo');
     }
-	
+
 	public function get_content() {
 		if ($this->content !== null) {
 		  return $this->content;
 		}
-	 
+
 		$helpdesk = '<h5 class=frontpage-heading>Learning Technologies Helpdesk</h5>
 		<p>Find us on Floor 2 of the Library</p>
 		<a class="twitter-timeline" data-height="600" data-link-color="#E81C4F" href="https://twitter.com/SolentLThelp?ref_src=twsrc%5Etfw"></a>
@@ -18,7 +18,7 @@ class block_frontpageinfo extends block_base {
 					<div id="succeed-container">
 					<a href="https://learn.solent.ac.uk/course/view.php?id=90">Succeed Home Page</a>
 					<br>
-					<h6>The Assignment Process:</h6>
+					<h5>The Assignment Process:</h6>
 					<ul class="succeed-list">
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31627">Starting Assignments</a></li>
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31629">Finding Sources</a></li>
@@ -30,7 +30,7 @@ class block_frontpageinfo extends block_base {
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31634">Dissertations and Major Projects</a></li>
 					</ul>
 
-					<h6>Professional Skills:</h6>
+					<h5>Professional Skills:</h6>
 					<ul class="succeed-list">
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31690">Group Work</a></li>
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31635">Managing Time and Stress</a></li>
@@ -39,45 +39,45 @@ class block_frontpageinfo extends block_base {
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31689">Maths and Statistics</a></li>
 						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31691">Professional Presence</a></li>
 					</ul></div>';
-					
+
 		//remove unrequired elements and code that causes errors
-		$librarytimes = file_get_contents('http://portal.solent.ac.uk/site-elements/templates/sub-templates/library-opening-hours-iframe-template-for-mycourse/library-opening-hours.aspx');		
+		$librarytimes = file_get_contents('http://portal.solent.ac.uk/site-elements/templates/sub-templates/library-opening-hours-iframe-template-for-mycourse/library-opening-hours.aspx');
 		$librarytimes = str_replace('<link href="/site-elements/css/site-styles2.css" rel="stylesheet" type="text/css" />',"", $librarytimes);
-		$librarytimes = str_replace('<link href="/site-elements/css/site-styles.css" rel="stylesheet" type="text/css" />',"", $librarytimes);					
+		$librarytimes = str_replace('<link href="/site-elements/css/site-styles.css" rel="stylesheet" type="text/css" />',"", $librarytimes);
 		$librarytimes = str_replace('<script src="/WebResource.axd?d=i128VoJtYp5a3QfcsHdbjMb2lfd7MH6uE1mCXhb2uJ-jMBgO3g7cW0MDBaEb3LPnBhK0zUwf7AqT-Prdr0oTug-5EKe4gzDYs5jmKusperk1&amp;t=634976343818095796" type="text/javascript"></script><noscript><p>Browser does not support script.</p></noscript>',"", $librarytimes);
-		$librarytimes = str_replace('theForm.submit = WebForm_SaveScrollPositionSubmit;',"", $librarytimes);		
+		$librarytimes = str_replace('theForm.submit = WebForm_SaveScrollPositionSubmit;',"", $librarytimes);
 		$librarytimes = str_replace('theForm.onsubmit = WebForm_SaveScrollPositionOnSubmit;',"", $librarytimes);
 		$librarytimes = str_replace('<script src="/ScriptResource.axd?d=Vj7KwkszshaerC1sAgbpx81vmKpJmyvkMX1hpTuMqpA2EiQoOmVVsvFj9VZ6mHWOV09kaOTLj_Ktoei5reXponZ_5xCxrscEZ_zstlIsCrHO6NJmNz701rIXfPJGUnJx1Tu_Xqfr5TdEoeFtF2oAtgYghHEHnC5t9plRzY9vmm01&amp;t=ffffffff940d030f" type="text/javascript"></script><noscript><p>Browser does not support script.</p></noscript>',"", $librarytimes);
 		$librarytimes = str_replace("<h3>Today's opening times</h3>","", $librarytimes);
 		//replace relative urls with full path
 		$librarytimes = str_replace('href="/library/essential-info/opening-hours/opening-hours.aspx">','href="http://portal.solent.ac.uk/library/essential-info/opening-hours/opening-hours.aspx" target="_blank">', $librarytimes);
 		$librarytimes = str_replace('href="/library/services-for/warsash-students/opening-hours.aspx">','href="http://portal.solent.ac.uk/library/services-for/warsash-students/opening-hours.aspx" target="_blank">', $librarytimes);
-			
+
 		$pcbooking = '<iframe width="100%" height="450px" src="https://mypc.solent.ac.uk/MyPC/Front.aspx?page=avMonitor&amp;language=en-GB&amp;refresh=30&amp;sid=3&amp;size=s&amp;limit=100&amp;cp=1&amp;tp=1" frameborder="0"></iframe>
 		<div><a href="https://mypc.solent.ac.uk/cire/login.aspx" target="_blank">Make a booking</a></div>
 		<div><a href="https://learn.solent.ac.uk/course/view.php?id=27658" target="_blank">myPC help and information</a></div><br>';
-			
+
 
 		$table = '	<div class="container">
 						<div class="row">
-							<div class="col-sm">' .
+							<div class="col-sm frontpage-block-left">' .
 								$helpdesk .'
 							</div>
-							<div class="col-sm">
+							<div class="col-sm frontpage-block-center">
 								<h5 class=frontpage-heading>Library</h5>' .
 								$librarytimes . '<hr>' . $pcbooking .'
 							</div>
-							<div class="col-sm succeed">' .
+							<div class="col-sm frontpage-block-right">' .
 								$succeed .'
 							</div>
 						</div>
-					</div>';		
-		
-		
+					</div>';
+
+
 
 		$this->content = new stdClass;
 		$this->content->text = $table;
-			 
+
 		return $this->content;
 	}
 }
