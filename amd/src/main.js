565c699cc4f97ab0fc621ba034218ea8e0d61a0d
Oliver Kent <o.jkent@gmail.com>
2019-06-20 09:49:32 +0100

    Prevented console error output when block is not on page

--- amd/src/main.js
+++ amd/src/main.js
@@ -1,24 +1,30 @@
 define(['jquery']);
 
-setInterval(function () {
-  var container = document.getElementById("computer_availability");
-  var content = container.innerHTML;
-  container.innerHTML= '';
-  $("#computer_availability").addClass("spinner fa fa-circle-o-notch fa-spin");
+var container = document.getElementById("computer_availability");
 
-  setTimeout(function () {
-    $.ajax({
-        url: '../blocks/frontpageinfo/externallib.php',
-        type: 'post',
-        data: { "callTableFunc": "1"},
-        success: function(response) {
-          container.innerHTML= response;
-        },
-       complete: function () {
-         $("#computer_availability").removeClass("spinner fa fa-circle-o-notch fa-spin");
-       }
-    });
+if (container) {
 
-  }, 1000);
+  setInterval(function () {
 
-}, 30000);
+    var content = container.innerHTML;
+    container.innerHTML= '';
+    $("#computer_availability").addClass("spinner fa fa-circle-o-notch fa-spin");
+
+    setTimeout(function () {
+      $.ajax({
+          url: '../blocks/frontpageinfo/externallib.php',
+          type: 'post',
+          data: { "callTableFunc": "1"},
+          success: function(response) {
+            container.innerHTML= response;
+          },
+         complete: function () {
+           $("#computer_availability").removeClass("spinner fa fa-circle-o-notch fa-spin");
+         }
+      });
+
+    }, 1000);
+
+  }, 30000);
+
+}
