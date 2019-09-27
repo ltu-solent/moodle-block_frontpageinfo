1510617b9f8240a92fa3c7007f7e7b42182ed7c9
Oliver Kent <o.jkent@gmail.com>
2019-09-27 15:54:14 +0100

    Changed layout, removed twitter feed and Succeed links

--- block_frontpageinfo.php
+++ block_frontpageinfo.php
@@ -39,37 +39,6 @@ class block_frontpageinfo extends block_base {
         return $table;
       }
 
-  		$helpdesk = '<h5 class=frontpage-heading>Learning Technologies Helpdesk</h5>
-  		<p>Find us on Floor 2 of the Library</p>
-  		<a class="twitter-timeline" data-height="735" data-link-color="#E81C4F" href="https://twitter.com/SolentLThelp?ref_src=twsrc%5Etfw"></a>
-  		<script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
-
-  		$succeed = '<h5 class=frontpage-heading>Succeed@Solent</h5>
-  					<div id="succeed-container">
-  					<a href="https://learn.solent.ac.uk/course/view.php?id=90">Succeed Home Page</a>
-  					<br><br>
-  					<h5>The Assignment Process:</h6>
-  					<ul class="succeed-list">
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31627">Starting Assignments</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31629">Finding Sources</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31628">Using Sources</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31630">Critical Thinking and Planning</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31631">Writing an Assignment</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31632">Referencing</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31636">Feedback and Reflection</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31634">Dissertations and Major Projects</a></li>
-  					</ul>
-
-  					<h5>Professional Skills:</h6>
-  					<ul class="succeed-list">
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31690">Group Work</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31635">Managing Time and Stress</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31637">Giving Presentations</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31649">Grammar and Language Skills</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31689">Maths and Statistics</a></li>
-  						<li><a href="https://learn.solent.ac.uk/course/view.php?id=31691">Professional Presence</a></li>
-  					</ul></div>';
-
   		//remove unrequired elements and code that causes errors
   		$librarytimes = file_get_contents('http://portal.solent.ac.uk/site-elements/templates/sub-templates/library-opening-hours-iframe-template-for-mycourse/library-opening-hours.aspx');
   		$librarytimes = str_replace('<link href="/site-elements/css/site-styles2.css" rel="stylesheet" type="text/css" />',"", $librarytimes);
@@ -93,18 +62,15 @@ class block_frontpageinfo extends block_base {
 
 
   		$table = '<div class="container">
-  						<div class="row">
-  							<div class="col-sm frontpage-block-left">' .
-  								$helpdesk .'
-  							</div>
-  							<div class="col-sm frontpage-block-center">
+  							<div class="col-sm frontpage-block-library">
   								<h5 class=frontpage-heading>Library</h5>' .
-  								$librarytimes . '<hr>' . '<h5 class=frontpage-heading>MyPC</h5>' .
+  								$librarytimes .
+                  '</div>' .
+                  '<div class="col-sm frontpage-block-pc">' .
+                  '<h5 class=frontpage-heading>MyPC</h5>' .
                   $pcbooking .'
-  							</div>
-  							<div class="col-sm frontpage-block-right">' .
-  								$succeed .'
-  							</div>
+                  </div>
+                </div>
   						</div>
   					</div>';
 
