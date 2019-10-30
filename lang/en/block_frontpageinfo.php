5267e1a76218a04ad68b081eaa8775171bab3ac4
Sarah Cotton <sarah.cotton@solent.ac.uk>
2019-10-30 15:23:32 +0000

    Remove all content except MyPC

--- lang/en/block_frontpageinfo.php
+++ lang/en/block_frontpageinfo.php
@@ -1,5 +1,5 @@
 <?php
 $string['pluginname'] = 'Front page info';
-$string['frontpageinfo'] = '';
+$string['frontpageinfo'] = 'MyPC';
 $string['frontpageinfo:addinstance'] = 'Add a new Library Opening Times block';
 $string['frontpageinfo:myaddinstance'] = 'Add a new Library Opening Times block to the My Moodle page';
